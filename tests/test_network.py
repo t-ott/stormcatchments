69200c74b079be30a3d36222428e8ff7f7cce67c
Tommy Ott <tommy.ott617@gmail.com>
2022-09-25 12:43:03 -0400

    Fix testing fixture name

--- tests/test_network.py
+++ tests/test_network.py
@@ -15,51 +15,51 @@ def net_johnson():
   return net
 
 
-def test_add_upstream_simple_johnson(net):
-  downstream_pt = net.pts[net.pts['OBJECTID']==244244]
-  net.add_upstream_pts(downstream_pt)
-  net.draw_G()
+def test_add_upstream_simple_johnson(net_johnson):
+  downstream_pt = net_johnson.pts[net_johnson.pts['OBJECTID']==244244]
+  net_johnson.add_upstream_pts(downstream_pt)
+  net_johnson.draw_G()
   print('Getting geom...')
-  print(net.G[244244])
+  print(net_johnson.G[244244])
   # print(nx.get_node_attributes(net.G, 'geometry')[0])
 
-  assert len(net.G.nodes()) == 3
+  assert len(net_johnson.G.nodes()) == 3
 
-def test_add_upstream_complex_johnson(net):
-  downstream_pt = net.pts[net.pts['OBJECTID']==21134]
-  net.add_upstream_pts(downstream_pt)
+def test_add_upstream_complex_johnson(net_johnson):
+  downstream_pt = net_johnson.pts[net_johnson.pts['OBJECTID']==21134]
+  net_johnson.add_upstream_pts(downstream_pt)
 
   # TODO:
   # How many nodes are there supposed to be?
   
 
-def test_add_upstream_twice_johnson(net):
+def test_add_upstream_twice_johnson(net_johnson):
   # 5 nodes
-  downstream_pt = net.pts[net.pts['OBJECTID']==21134]
-  net.add_upstream_pts(downstream_pt)
+  downstream_pt = net_johnson.pts[net_johnson.pts['OBJECTID']==21134]
+  net_johnson.add_upstream_pts(downstream_pt)
 
   # 4 nodes, disconnected from previous subgraph
-  downstream_pt = net.pts[net.pts['OBJECTID']==244153]
-  net.add_upstream_pts(downstream_pt)
+  downstream_pt = net_johnson.pts[net_johnson.pts['OBJECTID']==244153]
+  net_johnson.add_upstream_pts(downstream_pt)
 
-  # assert len(net.G.nodes()) == 9
-  # nx.draw(net.G)
+  # assert len(net_johnson.G.nodes()) == 9
+  # nx.draw(net_johnson.G)
   # plt.show()
 
 
-def test_get_outlet_johnson(net):
-  downstream_pt = net.pts[net.pts['OBJECTID']==21134]
-  net.add_upstream_pts(downstream_pt)
-  assert net.get_outlet(20847) == 21134
+def test_get_outlet_johnson(net_johnson):
+  downstream_pt = net_johnson.pts[net_johnson.pts['OBJECTID']==21134]
+  net_johnson.add_upstream_pts(downstream_pt)
+  assert net_johnson.get_outlet(20847) == 21134
 
 
-def test_find_downstream_simple_johnson(net):
-  upstream_pt = net.pts[net.pts['OBJECTID']==245051]
-  downstream_pt = net.find_downstream_pt(upstream_pt)
+def test_find_downstream_simple_johnson(net_johnson):
+  upstream_pt = net_johnson.pts[net_johnson.pts['OBJECTID']==245051]
+  downstream_pt = net_johnson.find_downstream_pt(upstream_pt)
   assert downstream_pt.OBJECTID == 244132
 
 
-def test_generate_catchment_graphs_johnson(net):
-  initial_catchment = gpd.read_file('tests/test_data/initial_catchment.shp')
-  net.generate_catchment_graphs(initial_catchment['geometry'])
-  print(net.G.nodes())
+def test_generate_catchment_graphs_johnson(net_johnson):
+  initial_catchment = gpd.read_file('tests/test_data/johnson_vt/initial_catchment.shp')
+  net_johnson.generate_catchment_graphs(initial_catchment['geometry'])
+  print(net_johnson.G.nodes())
