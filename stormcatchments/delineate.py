f5a1b04d13d2e506fac736f5a3064f74fa62489a
Tommy Ott <tommy.ott617@gmail.com>
2022-10-09 09:07:35 -0400

    annotation updates, move get_catchment out of class

--- stormcatchments/delineate.py
+++ stormcatchments/delineate.py
@@ -1,18 +1,74 @@
 import copy
 import geopandas as gpd
 import numpy as np
-import pysheds, pysheds.sgrid
+import pysheds, pysheds.sgrid, pysheds.sview
 from shapely.geometry import Polygon
 
 from stormcatchments.network import Network
+
+def get_catchment(
+  pour_pt: tuple,
+  grid: pysheds.sgrid.sGrid,
+  fdir: pysheds.sview.Raster,
+  acc: pysheds.sview.Raster,
+  grid_epsg: int,
+  acc_thresh: int=1000
+) -> gpd.GeoDataFrame:
+    '''
+    Delineate catchment using pysheds
+
+    Parameters
+    ----------
+    pour_pt: tuple
+      An (x, y) coordinate pair, with the same coordinate system as the grid
+
+    grid: pysheds.sgrid.sGrid
+      DEM
+    
+    fdir: pysheds.sview.Raster
+      A pysheds flow direction raster
+
+    acc: pysheds.sview.Raster
+      A pysehds flow accumulation raster
+
+    grid_epsg: int
+      EPSG code for the CRS of the DEM
+
+    acc_thresh: int (default 1000)
+      The minimum accumulation threshold used during pour point snapping
+    
+    Returns
+    -------
+    catchment: gpd.GeoDataFrame
+      A GeoDataFrame containing the newly delineated catchment polygon
+    '''
+    # create a deep copy of the grid to not manipulate original grid
+    grid = copy.deepcopy(grid)
+    x, y = pour_pt
+    x_snap, y_snap = grid.snap_to_mask(acc > acc_thresh, (x, y))
+    catch = grid.catchment(x=x_snap, y=y_snap, fdir=fdir)
+    grid.clip_to(catch)
+    catch_view = grid.view(catch, dtype=np.uint8)
+    catch_vec = grid.polygonize(catch_view)
+    
+    catch_polys = []
+    for shape, _ in catch_vec:
+        assert shape['type'] == 'Polygon'
+        all_poly_coords = shape['coordinates']
+        for poly_coords in all_poly_coords:
+            catch_polys.append(Polygon(poly_coords))
+  
+    return gpd.GeoDataFrame(
+      {'geometry': gpd.GeoSeries(catch_polys).set_crs(epsg=grid_epsg)}
+    )
 
 class Delineate:
   def __init__(
       self,
       network: Network,
       grid: pysheds.sgrid.sGrid,
-      fdir: pysheds.sgrid.sGrid,
-      acc: pysheds.sgrid.sGrid,
+      fdir: pysheds.sview.Raster,
+      acc: pysheds.sview.Raster,
       grid_epsg: int,
     ):
     '''
@@ -23,11 +79,11 @@ class Delineate:
     grid: pysheds.sgrid.sGrid
       DEM
 
-    acc: pysheds.sview.Raster
-      A pysehds flow accumulation raster
-
     fdir: pysheds.sview.Raster
       A pysheds flow direction raster
+
+    acc: pysheds.sview.Raster
+      A pysehds flow accumulation raster
 
     grid_epsg: int
       EPSG code for the CRS of the DEM
@@ -55,25 +111,7 @@ class Delineate:
     catchment: gpd.GeoDataFrame
       A GeoDataFrame containing the newly delineated catchment polygon
     '''
-    # create a deep copy of the grid to not manipulate original grid
-    grid = copy.deepcopy(self.grid)
-    x, y = pour_pt
-    x_snap, y_snap = grid.snap_to_mask(self.acc > acc_thresh, (x, y))
-    catch = grid.catchment(x=x_snap, y=y_snap, fdir=self.fdir)
-    grid.clip_to(catch)
-    catch_view = grid.view(catch, dtype=np.uint8)
-    catch_vec = grid.polygonize(catch_view)
-    
-    catch_polys = []
-    for shape, _ in catch_vec:
-        assert shape['type'] == 'Polygon'
-        all_poly_coords = shape['coordinates']
-        for poly_coords in all_poly_coords:
-            catch_polys.append(Polygon(poly_coords))
-  
-    return gpd.GeoDataFrame(
-      {'geometry': gpd.GeoSeries(catch_polys).set_crs(epsg=self.grid_epsg)}
-    )
+    return get_catchment(pour_pt, self.grid, self.fdir, self.acc, self.grid_epsg)
 
   def delineate_points(
     self, pts: gpd.GeoDataFrame, delineated_oids: set
