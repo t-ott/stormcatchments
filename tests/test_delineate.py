118bcd2d3230ef190db2c2fd0cb4a4dd5e2cc9d5
Tommy Ott <tommy.ott617@gmail.com>
2022-10-10 14:41:14 -0400

    Complete new iteration of get_stormcatchment

--- tests/test_delineate.py
+++ tests/test_delineate.py
@@ -1,5 +1,5 @@
 import geopandas as gpd
-from pysheds.grid import Grid
+from shapely.geometry import Point
 import pytest
 
 from stormcatchments import network, delineate, terrain
@@ -17,10 +17,21 @@ def delineate_johnson():
   return delineate.Delineate(net, grid, fdir, acc, 6589)
 
 def test_get_catchment(delineate_johnson):
+  delin = delineate_johnson
   pour_pt = (484636, 237170)
-  catchment = delineate_johnson.get_catchment(pour_pt)
+  catchment = delin.get_catchment(pour_pt)
   assert round(catchment.area.values[0], 1) == 6796.3
 
 def test_get_stormcatchment(delineate_johnson):
+  delin = delineate_johnson
   pour_pt = (484636, 237170)
-  stormcatchment = delineate_johnson.get_stormcatchment(pour_pt)
+  stormcatchment = delin.get_stormcatchment(pour_pt)
+  stormcatchment_geom = stormcatchment.iloc[0].geometry
+
+  contain_coords = [(484700, 237490), (484696, 237272)]
+  for coords in contain_coords:
+    assert stormcatchment_geom.contains(Point(coords))
+
+  not_contain_coords = [(484590, 237200), (484750, 237330)]
+  for coords in not_contain_coords:
+    assert not stormcatchment_geom.contains(Point(coords))
