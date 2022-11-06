26afe33473a280ce9a65e0c5b82399ae65db7aa3
t-ott <68197166+t-ott@users.noreply.github.com>
2022-11-06 09:19:28 -0500

    Add find_packges to setup

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
   long_description = f.read()
 
 setup(
   name='stormcatchments',
-  version='0.1.1',
+  version='0.1.2',
   description='Stormwater network aware catchment delineation',
   long_description=long_description,
   long_description_content_type='text/markdown',
   author='Thomas Ott',
   author_email='tommy.ott617@gmail.com',
+  license='MIT',
   url='https://github.com/t-ott/stormcatchments',
   py_modules=[
     'stormcatchments'
   ],
+  packages=find_packages(),
   install_requires=[
     'geopandas',
     'networkx',
