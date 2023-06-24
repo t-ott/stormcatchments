773c79dd7541dd1ffc4469bbc33b1441caf03ed9
Tommy Ott <tommy.ott617@gmail.com>
2023-06-24 12:24:58 -0400

    Update gitignore and change pt mapping in example script

--- stormcatchments/00_directioned_lines.py
+++ stormcatchments/00_directioned_lines.py
@@ -4,9 +4,10 @@ from network import Network
 DATA_GPKG = "/Users/TommyOtt/GIS/_data/GIS_SET_5/GIS_SET_5.gpkg"
 lines = gpd.read_file(DATA_GPKG, layer="SW_ClosedConduit", rows=1000)
 pts = gpd.read_file(DATA_GPKG, layer="SW_Inlet", rows=1000)
+pts['IS_SINK'] = True
+pts['IS_SOURCE'] = False
 
 net = Network(
     storm_lines=lines,
     storm_pts=pts,
-    type_column="SUBTYPE"
 )
